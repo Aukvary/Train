e52af8d03c8f2d560788557f11b53d9e24c68cad
Aukvary <al3xpop123@gmail.com>
2024-05-30 09:18:47 +0700

    small refab

--- Assets/GeneralScpits/DataManager.cs
+++ Assets/GeneralScpits/DataManager.cs
@@ -4,13 +4,11 @@ using UnityEngine.SceneManagement;
 
 public static class DataManager
 {
-    private static List<Item> _items;
     private static Vector3 _lastPosition =  Vector3.zero;
     private static string _currentEnemy;
 
     public static Dictionary<string, bool> EnemyTrains = new Dictionary<string, bool>();
 
-    public static List<Item> Items => _items;
     public static Vector3 LastPosition => _lastPosition;
 
     public static void GoToFightScene(InventorySystem invenory, string train)
@@ -33,14 +31,17 @@ public static class DataManager
             EnemyTrains[_currentEnemy] = false;
         }
         else
+        {
             EnemyTrains[_currentEnemy] = true;
+            _lastPosition = Vector3.zero;
+        }
         _currentEnemy = null;
         SceneManager.LoadScene((int)Scenes.MapScene);
     }
 
     public static void DeleteData()
     {
-        _items = null;
+        EnemyTrains.Clear();
         _lastPosition = Vector3.zero;
     }
 }
